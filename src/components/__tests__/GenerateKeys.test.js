8c36e69c20d7b6f2f8aa83d263191911b603aaac
Sagar Gopale <sagar090989@gmail.com>
2022-11-11 19:33:23 +0530

    Added engine in package

--- src/components/__tests__/GenerateKeys.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen, cleanup } from '@testing-library/react';
-import GenerateKeys from '../GenerateKeys';
-
-test('should render GenerateKeysFormComponent', () => {
-    render(<GenerateKeys/>);
-    const generateKeysElement = screen.getByTestId('generateKeysFormComponent');
-    expect(generateKeysElement).toBeInTheDocument();
-});
